586d44f75dba0c52632b6376022dc3cf49d6684d
Md Sahil <86847852+mksahil@users.noreply.github.com>
2025-08-12 16:03:31 +0530

    Create altFuture.py

---
+++ app/altFuture.py
@@ -0,0 +1,44 @@
+{
+  "IsSuccess": true,
+  "Message": "SUCCESS",
+  "ErrorMessage": "",
+  "MatchedCnt" : int
+  "ResponseData": {
+    "MatchedIdeas": [
+      {
+        "IdeaId": 3,
+        "SimilarityPercent": int 
+      },
+      {
+        "IdeaId": 3,
+        "SimilarityPercent": int
+      },{
+        "IdeaId": 3,
+        "SimilarityPercent": int
+      }]}}
+
+
+
+"OldIdeas": [
+     {
+       "Id": 3,
+       "Title": "Submitted",
+       "Desc": 9
+     },
+     {
+      "Id": 3,
+       "Title": "Submitted",
+       "Desc": 9
+     } ],
+"NewIdea": [
+     {
+       "Id": 3,
+       "Title": "Submitted",
+       "Desc": 9
+     },
+     {
+      "Id": 3,
+       "Title": "Submitted",
+       "Desc": 9
+     } ] 
+}   
